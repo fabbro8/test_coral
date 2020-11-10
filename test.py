cfc40ca4758e24a52d6faacdd2ea0a60466983de
fabbro8 <73879602+fabbro8@users.noreply.github.com>
2020-11-10 16:34:24 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+from periphery import GPIO
+
+# Open GPIO's
+gpio6 = GPIO(6, "in")
+gpio7 = GPIO(7, "in")
+gpio8 = GPIO(8, "in")
+gpio138 = GPIO(138, "in")
+gpio140 = GPIO(140, "in")
+gpio141 = GPIO(141, "in")
+gpio73 = GPIO(73, "out")
+gpio77 = GPIO(77, "out")
+
+# Snapshot of an input at a moment in time
+GPIO.read(gpio6) # True for high state, False for low state
+
